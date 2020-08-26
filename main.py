01c2f926d1698b068d7315770e24c54d00733559
Arunima199 <70276334+Arunima199@users.noreply.github.com>
2020-08-27 03:27:54 +0530

    Change Boundary Condition

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
 max_value=int(input("enter the charge threshold for your laptops battery(in %):\t"))
-if(max_value<100 and max_value>0):
+if(max_value<=100 and max_value>0):
     p=os.system(f'echo {max_value} |sudo tee /sys/class/power_supply/BAT0/charge_control_end_threshold')
 else:
     print('Invalid input')
